a117f85f0588508e6dc2a2f9c5cd1ca5525c74e5
Sotatek-NghiaHo <https://github.com/Sotatek-NghiaHo>
2025-10-02 08:56:09 +0700

    update

--- Openshift_env/Chapter8_1-Comprehensive/create-frontendspa.sh
+++ Openshift_env/Chapter8_1-Comprehensive/create-frontendspa.sh
@@ -2,10 +2,10 @@
 
 cd ~/D0288/labs/compreview-todo/todo-frontend
 
-sed -i '291\\nEXPOSE 8080\n\nUSER nginx' Containerfile
+sed -i '29i\\nEXPOSE 8080\n\nUSER nginx' Containerfile
 
 podman login -u developer -p developer registry.ocp4.example.com:8443
-podman build-t registry.ocp4.example.com:8443/developer/todo-frontend:latest
+podman build-t registry.ocp4.example.com:8443/developer/todo-frontend:latest .
 podman push registry.ocp4.example.com:8443/developer/todo-frontend
 
 oc login -u developer -p developer https://api.ocp4.example.com:6443
--- Openshift_env/Chapter8_2-Comprehensive/8_2_main.md
+++ Openshift_env/Chapter8_2-Comprehensive/8_2_main.md
@@ -9,4 +9,26 @@ total 12
 
 ![alt text](pic/1.png)
 
+![alt text](pic/2.png)
+
+![alt text](pic/3.png)
+
+![alt text](pic/4.png)
+
+```
+# sau khi sua xong
+oc apply -f pipeline.yaml
+tkn pr delete words-cicd-pipeline-run-6cqrs 
+tkn clustertask list | grep buildah
+tkn pr list
+tkn task list
+```
+
+![alt text](pic/5.png)
+
+![alt text](pic/6.png)
+
+```
+oc get task buildah -n openshift-pipelines -o yaml > buildah-task.yaml
+```
 
