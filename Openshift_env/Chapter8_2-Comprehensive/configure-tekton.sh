f8ceef0a43ac7708bf5f997c449e2d906e5bb99f
Sotatek-NghiaHo <https://github.com/Sotatek-NghiaHo>
2025-10-14 11:14:16 +0700

    update

--- Openshift_env/Chapter8_2-Comprehensive/configure-tekton.sh
+++ Openshift_env/Chapter8_2-Comprehensive/configure-tekton.sh
@@ -6,8 +6,6 @@ oc login -u developer -p developer https://api.ocp4.example.com:6443
 
 oc project compreview-cicd
 
-oc apply -f basic-user-pass.yaml
+oc secrets link pipeline basic-user-pass --for=pull,mount
 
-oc secret link pipeline basic-user-pass
-
-tkn pipeline start --use-param-defaults words-cicd-pipeline -p APP_PATH=apps/compreview-cicd/words -w name=shared, volumeClaimTemplateFile=volume-template.yaml
+tkn pipeline start --use-param-defaults words-cicd-pipeline -p APP_PATH=apps/compreview-cicd/words -w name=shared,volumeClaimTemplateFile=volume-template.yaml
